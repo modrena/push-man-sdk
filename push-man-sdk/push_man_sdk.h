36ab29b4e61c9e38c99894d97a1d3a1c13392210
maxwesel <max@toralarm.com>
2024-12-30 13:08:58 +0100

    Initial Commit

---
+++ push-man-sdk/push_man_sdk.h
@@ -0,0 +1,18 @@
+//
+//  push_man_sdk.h
+//  push-man-sdk
+//
+//  Created by Max Wesel on 30.12.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for push_man_sdk.
+FOUNDATION_EXPORT double push_man_sdkVersionNumber;
+
+//! Project version string for push_man_sdk.
+FOUNDATION_EXPORT const unsigned char push_man_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <push_man_sdk/PublicHeader.h>
+
+
---
+++ push-man-sdkTests/push_man_sdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  push_man_sdkTests.swift
+//  push-man-sdkTests
+//
+//  Created by Max Wesel on 30.12.24.
+//
+
+import Testing
+@testable import push_man_sdk
+
+struct push_man_sdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
